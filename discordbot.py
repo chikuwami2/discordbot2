99f5fcc3e92166c6855a710aabde82b7feea3665
chikuwami <acspot122@gmail.com>
2020-07-17 21:44:39 +0900

    cliant化２

--- discordbot.py
+++ discordbot.py
@@ -3,46 +3,32 @@ import asyncio
 import random
 import sys
 import os
-import botFunction.functions as f
 
 client = discord.Client()
-
-func_list = {
-    '名言': f.random_meigen,
-    '迷言': f.random_meigen,
-    '武器': f.random_splat_buki,
-    'ブキ': f.random_splat_buki,
-    'help': f.help,
-    'ヘルプ': f.help
-}
 
 
 @client.event
 async def on_ready():
-    '''
-    起動時に呼ばれるメソッド
-    '''
     print('-----Logged in info-----')
     print(client.user.name)
     print(client.user.id)
     print('------------------------')
 
 
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    '''
-    特定のメッセージを受け取って処理する\n
-    今はメンションを送るとランダムに名言を返す
-    '''
-    try:
-        if client.user.id in message.content:
-            for k in func_list:
-                if k in str(message.content):
-                    await func_list[k](client, message)
-                    break
-            else:
-                await f.random_reply(client, message)
-    except:
-        print(sys.exc_info())
+    test_blacklist = 0
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    if message.content == '/test blacklist':
+        test_blacklist = 1
+        await message.channel.send('ブラックリストかどうか検知するよ！！')
+
+    if message.content == 'わんこ！':
+        await message.channel.send(f'{message.author.mention} なーに？？(｀･ω･´)✨')
+
 
 client.run(os.environ.get('ENV_VAR_DISCORD_ID'))
