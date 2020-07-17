ed9c2f3e24b22b04bb2bdcc3ebc4363a98e18872
chikuwami <acspot122@gmail.com>
2020-07-17 18:16:39 +0900

    test4

--- discordbot.py
+++ discordbot.py
@@ -2,20 +2,16 @@ from discord.ext import commands
 import os
 import traceback
 
+client = discord.Client()
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)
