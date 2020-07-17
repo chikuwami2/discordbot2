afb9021e90a77ef58781612f2496367dd7dba221
chikuwami <acspot122@gmail.com>
2020-07-17 19:49:09 +0900

    ミス修正

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,4 @@
-from discord.ext import commands
-import os
-import traceback
-
-client = discord.Client()
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-
-client.run(token)import discord
+import discord
 import asyncio
 import random
 import sys
