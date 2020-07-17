93cefe93092cef3e10a572f174922f122949afa0
chikuwami <acspot122@gmail.com>
2020-07-17 19:22:26 +0900

    初期化

--- discordbot.py
+++ discordbot.py
@@ -2,16 +2,20 @@ from discord.ext import commands
 import os
 import traceback
 
-client = discord.Client()
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
-client.run(token)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
